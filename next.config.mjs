50eb83bd66ff8b07e7be7c52ca10588290b58fb5
FloreDenis2001 <floredenis907@yahoo.com>
2024-10-17 17:43:31 +0300

    push

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,23 @@
-import withVideos from 'next-videos';
-
 /** @type {import('next').NextConfig} */
-const nextConfig = withVideos({
-  webpack(config, options) {
-    return config;
-  },
-  images: {
-    domains: ['s3-us-west-2.amazonaws.com', 'via.placeholder.com'],
-  },
-});
-
-export default nextConfig;
+const nextConfig = {
+    webpack(config, options) {
+      config.module.rules.push({
+        test: /\.(mp4|webm|ogg|swf|ogv)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next/static/videos/',
+            outputPath: 'static/videos/',
+            name: '[name].[hash].[ext]',
+          },
+        },
+      });
+      return config;
+    },
+    images: {
+      domains: ['s3-us-west-2.amazonaws.com', 'via.placeholder.com'],
+    },
+  };
+  
+  export default nextConfig;
+  
