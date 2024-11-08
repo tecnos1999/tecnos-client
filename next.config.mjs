0c198863a878d038033b42044e0a21e0e7e59676
FloreDenis2001 <floredenis907@yahoo.com>
2024-11-08 22:39:33 +0200

    update

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
             use: {
                 loader: 'file-loader',
                 options: {
-                    publicPath: `${process.env.REACT_APP_ENV === 'development' ? 'http://localhost:8080/ui-static' : 'http://tecnos-gateway/ui-static'}/videos/`,
+                    publicPath: `${process.env.REACT_APP_ENV === 'development' ? 'http://localhost:8080/ui-static' : 'http://84.46.241.251:8080/ui-static'}/videos/`,
                     outputPath: 'static/videos/',
                     name: '[name].[hash].[ext]',
                 },
@@ -17,8 +17,8 @@ const nextConfig = {
         domains: ['s3-us-west-2.amazonaws.com', 'via.placeholder.com'],
     },
     assetPrefix: process.env.REACT_APP_ENV === 'development'
-        ? 'http://localhost:80/ui-static'
-        : 'http://84.46.241.251/ui-static',
+        ? 'http://localhost:8080/ui-static'
+        : 'http://84.46.241.251:8080/ui-static',
 
 };
 
