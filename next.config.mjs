7dbc8b392bb65a71b7de06bae4b85ca19c8062c7
FloreDenis2001 <floredenis907@yahoo.com>
2024-11-10 16:07:31 +0200

    up

--- next.config.mjs
+++ next.config.mjs
@@ -18,7 +18,7 @@ const nextConfig = {
     },
     assetPrefix: process.env.REACT_APP_ENV === 'development'
         ? 'http://localhost:8080/ui-static'
-        : 'https://89.33.44.227:8080/ui-static',
+        : 'https://89.33.44.227/ui-static',
 
 };
 
