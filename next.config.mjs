a716590fe2f484d210f9fe39059fbadc873814cd
FloreDenis2001 <floredenis907@yahoo.com>
2024-11-24 21:49:58 +0200

    up

--- next.config.mjs
+++ next.config.mjs
@@ -18,14 +18,13 @@ const nextConfig = {
     images: {
         domains: ['s3-us-west-2.amazonaws.com', 'via.placeholder.com', 'localhost', '**'],
     },
-
     
     assetPrefix: process.env.REACT_APP_ENV === 'development'
         ? 'http://localhost:8080/ui-static'
         : 'https://89.33.44.227/ui-static',
   
 
-        
+
     reactStrictMode: true,
 };
 
