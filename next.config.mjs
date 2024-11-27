65fed451a72499543f24aabd39e2cf301045cb1b
FloreDenis2001 <floredenis907@yahoo.com>
2024-11-27 23:54:02 +0200

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,31 @@
-const isDevelopment = process.env.NODE_ENV === 'development';
-
 const nextConfig = {
-  basePath: '/ui',
-  assetPrefix: '/ui-static',
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.(mp4|webm|ogg|swf|ogv)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          publicPath: '/ui-static/videos/',
-          outputPath: 'static/videos/',
-          name: '[name].[hash].[ext]',
-        },
-      },
-    });
-    return config;
-  },
-  images: {
-    domains: ['s3-us-west-2.amazonaws.com', 'via.placeholder.com', 'localhost'],
-  },
-  reactStrictMode: true,
+    webpack(config, options) {
+        config.module.rules.push({
+            test: /\.(mp4|webm|ogg|swf|ogv)$/,
+            use: {
+                loader: 'file-loader',
+                options: {
+                    publicPath: `${process.env.REACT_APP_ENV === 'development' ? 'http://localhost:8080/ui-static' : 'https://89.33.44.227/ui-static'}/videos/`,
+                    outputPath: 'static/videos/',
+                    name: '[name].[hash].[ext]',
+                },
+            },
+        });
+
+        
+        return config;
+    },
+    images: {
+        domains: ['s3-us-west-2.amazonaws.com', 'via.placeholder.com', 'localhost', '**'],
+    },
+    
+    assetPrefix: process.env.REACT_APP_ENV === 'development'
+        ? 'http://localhost:8080/ui-static'
+        : 'https://89.33.44.227/ui-static',
+
+    
+    reactStrictMode: true,
+  
 };
 
-module.exports = nextConfig;
+export default nextConfig;
